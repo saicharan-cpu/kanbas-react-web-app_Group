fe1508ee1068605f5dcd4f58a0082c3547c689d9
Sai Charan Reddy <saicharanreddy@Sais-MacBook-Air.local>
2024-07-15 14:15:22 +0530

    working code till labs

---
+++ src/Labs/Lab5/QueryParameters.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+
+export default function QueryParameters() {
+  const [a, setA] = useState("34");
+  const [b, setB] = useState("23");
+
+  return (
+    <div id="wd-query-parameters">
+      <h3>Query Parameters</h3>
+      <input
+        id="wd-query-parameter-a"
+        className="form-control mb-2"
+        value={a}
+        type="number"
+        onChange={(e) => setA(e.target.value)}
+      />
+      <input
+        id="wd-query-parameter-b"
+        className="form-control mb-2"
+        value={b}
+        type="number"
+        onChange={(e) => setB(e.target.value)}
+      />
+      <div style={{ display: 'flex', gap: '10px', flexWrap: 'wrap' }}>
+        <a
+          id="wd-query-parameter-add"
+          href={`${REMOTE_SERVER}/lab5/calculator?operation=add&a=${a}&b=${b}`}
+        >
+          Add {a} + {b}
+        </a>
+        <a
+          id="wd-query-parameter-subtract"
+          href={`${REMOTE_SERVER}/lab5/calculator?operation=subtract&a=${a}&b=${b}`}
+        >
+          Subtract {a} - {b}
+        </a>
+        <a
+          id="wd-query-parameter-multiply"
+          href={`${REMOTE_SERVER}/lab5/calculator?operation=multiply&a=${a}&b=${b}`}
+        >
+          Multiply {a} * {b}
+        </a>
+        <a
+          id="wd-query-parameter-divide"
+          href={`${REMOTE_SERVER}/lab5/calculator?operation=divide&a=${a}&b=${b}`}
+        >
+          Divide {a} / {b}
+        </a>
+      </div>
+      <hr />
+    </div>
+  );
+}
